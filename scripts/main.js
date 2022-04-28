bdb218aa8520c0eadac84735c4684f2f7b5d6df1
Aaravssi <aaravbatra.ssi@gmail.com>
2022-04-28 21:48:13 +0530

    better mobile support

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,16 @@
-const myheading = document.querySelector('h1');
-myheading.textContent ="Hello World!";
-let Wow = "Tales Of the Farlands!";
-myheading.textContent = Wow;
-setTimeout(() => {
+/*const myheading = document.querySelector('h1'); // Declaring a constant called myheading which now contains h1
+myheading.textContent ="Hello World!"; // Attaching a new value to the text context of myheading, a constant
+let Wow = "Tales Of the Farlands!"; // A new value proving for it can later be changed, the command is under a comment because it serves no purpose and has a better alternative*
+myheading.textContent = Wow;*/ // It works!
+document.querySelector('h1').textContent = "Tales Of The Farlands!"; // Works, selecting the function then adding a dot in between to tell what to add
+/*setTimeout(() => {
     if(1+3%2===0){ // BODMAS applies
         alert('Why are you here?') // 1st Option
     }
     else{
         alert('One Wrong Turn and You can\'t turn around') // 2nd Option
     }
-}, 100);
-
+}, 100); // The 100 here is the time before it's executed
 function maths(num1, num2){ // The variables that will be later defined while function usage like : maths(1, 2)
     let wowboo = num2 * num1; // Declaring a variable containing the variables later decided which are multiplied
     let result = wowboo/num1; // Another calculation dividing the previous variable with the later defined variable
@@ -20,6 +20,13 @@ const answer = maths(1,2); // Declaring a variable containing the values of the 
 const answering = document.querySelector('#Answer') // Connecting the constant answering to the id #Answer
 answering.textContent = answer; // Filling the answer in the .textcontent
 function openwindow (){
-    window.open();
-}
-var a = !(12>14)? openwindow : alert('The operation was brought to a halt');
+    let windowFeatures = "popup";
+    let openwin = window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=3s", windowFeatures);
+    return openwin;
+} // Testing stage
+let a = (12>14)? openwindow : alert('The operation was brought to a halt');
+document.querySelector('html').addEventListener('click', () => {
+    (1+3%2===0)? alert('The statement "1+3%2===0" is true') : alert ('The statement "1+3%2===0" was proved to be false');
+    alert(1+419);
+})*/
+
